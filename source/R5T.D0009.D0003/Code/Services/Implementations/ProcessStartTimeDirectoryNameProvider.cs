6b485392d6c4f2fbb6528d45ecc5630e1d893b98
MinexAutomation <minex.automation@gmail.com>
2020-05-15 12:48:40 -0700

    Added R5T.D0009.D0003 library.

---
+++ source/R5T.D0009.D0003/Code/Services/Implementations/ProcessStartTimeDirectoryNameProvider.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Threading.Tasks;
+
+using R5T.D0003;
+using R5T.D0008;
+
+
+namespace R5T.D0009.D0003
+{
+    public class ProcessStartTimeDirectoryNameProvider : IProcessStartTimeDirectoryNameProvider
+    {
+        private IProcessStartTimeProvider ProcessStartTimeProvider { get; }
+        private ITimestampUtcDirectoryNameProvider TimestampUtcDirectoryNameProvider { get; }
+
+
+        public ProcessStartTimeDirectoryNameProvider(
+            IProcessStartTimeProvider processStartTimeProvider,
+            ITimestampUtcDirectoryNameProvider timestampUtcDirectoryNameProvider)
+        {
+            this.ProcessStartTimeProvider = processStartTimeProvider;
+            this.TimestampUtcDirectoryNameProvider = timestampUtcDirectoryNameProvider;
+        }
+
+        public async Task<string> GetProcessStartTimeDirectoryName()
+        {
+            var processStartTime = await this.ProcessStartTimeProvider.GetProcessStartTimeAsync();
+
+            var processStartTimeDirectoryName = await this.TimestampUtcDirectoryNameProvider.GetTimestampUtcDirectoryNameAsync(processStartTime);
+            return processStartTimeDirectoryName;
+        }
+    }
+}
