5b6a6332209399d140761d396f0c672dcc539467
MinexAutomation <minex.automation@gmail.com>
2020-05-15 15:45:36 -0700

    Removed UTC functionality, since that is *not* what this service is for (there will be an IProcessStartTimeUtcDirectoryNameProvider).

--- source/R5T.D0009.Base/Code/Services/Definitions/IProcessStartTimeDirectoryNameProvider.cs
+++ source/R5T.D0009.Base/Code/Services/Definitions/IProcessStartTimeDirectoryNameProvider.cs
@@ -7,6 +7,5 @@ namespace R5T.D0009
     public interface IProcessStartTimeDirectoryNameProvider
     {
         Task<string> GetProcessStartTimeDirectoryName();
-        Task<string> GetProcessStartTimeUtcDirectoryName();
     }
 }
