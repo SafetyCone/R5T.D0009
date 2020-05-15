4f56c792cef8b12040e8925d0a1ac75ecf0ab4c5
MinexAutomation <minex.automation@gmail.com>
2020-05-15 12:38:06 -0700

    Added R5T.D0009.Base library. Added IProcessStartTimeDirectoryNameProvider service definition.

---
+++ source/R5T.D0009.Base/Code/Services/Definitions/IProcessStartTimeDirectoryNameProvider.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Threading.Tasks;
+
+
+namespace R5T.D0009
+{
+    public interface IProcessStartTimeDirectoryNameProvider
+    {
+        Task<string> GetProcessStartTimeDirectoryName();
+    }
+}
