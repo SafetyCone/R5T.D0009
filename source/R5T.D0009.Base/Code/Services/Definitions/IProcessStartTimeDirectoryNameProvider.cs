0db2668f4879cc9d798bcf4b4f1a49f6c6285ccc
davidcoats <david@getsafetycone.com>
2022-04-27 15:26:34 -0700

    Interim changes.

--- source/R5T.D0009.Base/Code/Services/Definitions/IProcessStartTimeDirectoryNameProvider.cs
+++ source/R5T.D0009.Base/Code/Services/Definitions/IProcessStartTimeDirectoryNameProvider.cs
@@ -1,10 +1,13 @@
 ﻿using System;
 using System.Threading.Tasks;
 
+using R5T.T0064;
+
 
 namespace R5T.D0009
 {
-    public interface IProcessStartTimeDirectoryNameProvider
+    [ServiceDefinitionMarker]
+    public interface IProcessStartTimeDirectoryNameProvider : IServiceDefinition
     {
         Task<string> GetProcessStartTimeDirectoryName();
     }
--- source/R5T.D0009.D0003/Code/Services/Implementations/ProcessStartTimeDirectoryNameProvider.cs
+++ source/R5T.D0009.D0003/Code/Services/Implementations/ProcessStartTimeDirectoryNameProvider.cs
@@ -3,11 +3,13 @@ using System.Threading.Tasks;
 
 using R5T.D0003;
 using R5T.D0008;
+using R5T.T0064;
 
 
 namespace R5T.D0009.D0003
 {
-    public class ProcessStartTimeDirectoryNameProvider : IProcessStartTimeDirectoryNameProvider
+    [ServiceImplementationMarker]
+    public class ProcessStartTimeDirectoryNameProvider : IProcessStartTimeDirectoryNameProvider, IServiceImplementation
     {
         private IProcessStartTimeProvider ProcessStartTimeProvider { get; }
         private ITimestampUtcDirectoryNameProvider TimestampUtcDirectoryNameProvider { get; }
